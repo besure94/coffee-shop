5b420adbf6c400b4bd64777a6507b82dc919f8ff
Brian Scherner <brianscherner@gmail.com>
2024-03-20 15:36:37 -0700

    Import bootstrap into project.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './components/index.css';
 import App from './components/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
