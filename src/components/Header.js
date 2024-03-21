1a486bc677fa561ad4b15373c445b3ee42dd87b1
Brian Scherner <brianscherner@gmail.com>
2024-03-21 10:45:27 -0700

    Add image to application.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import coffeeImage from "./../img/coffee-pic.jpg";
 
 function Header() {
   return (
     <div className="app-header">
       <h1>Coffee Shop</h1>
+      <img src={coffeeImage} alt=""/>
     </div>
   );
 }
